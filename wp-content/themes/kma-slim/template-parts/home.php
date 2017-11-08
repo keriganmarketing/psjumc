3bb4d610477832fbe5a8ce3647d6d188d13ebb2a
Jack Kerigan <support@kerigan.com>
2017-11-08 13:02:50 -0500

    started template customizations

--- wp-content/themes/kma-slim/template-parts/home.php
+++ wp-content/themes/kma-slim/template-parts/home.php
@@ -15,14 +15,38 @@ include(locate_template('template-parts/sections/top.php'));
 ?>
 <div id="mid" >
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <div class="section" >
+        <div class="section home-header" >
 
-            <bulma-slider>
-                    <?php
-                        $slider = new BulmaSlider();
-                        echo $slider->getSlider('home-page-slider');
-                    ?>
-            </bulma-slider>
+            <div class="container">
+
+                <div class="columns is-multiline">
+                    <div class="column is-8-desktop">
+                        <bulma-slider>
+                            <?php
+                            $slider = new BulmaSlider();
+                            echo $slider->getSlider('home-page-slider');
+                            ?>
+                        </bulma-slider>
+                    </div>
+                    <div class="column is-12 is-4-desktop">
+                        <div class="columns is-multiline">
+                            <div class="column is-6 is-12-desktop">
+                                <img src="https://bulma.io/images/placeholders/480x320.png" style="display: block; width: 100%;">
+                            </div>
+                            <div class="column is-6 is-12-desktop">
+                                <img src="https://bulma.io/images/placeholders/480x320.png" style="display: block; width: 100%;">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+        <div class="section content <?= $post->post_name; ?>">
+
+        </div>
+        <div class="section connect">
 
         </div>
     </article>
--- wp-content/themes/kma-slim/template-parts/sections/top.php
+++ wp-content/themes/kma-slim/template-parts/sections/top.php
@@ -10,7 +10,7 @@ use Includes\Modules\Navwalker\BulmaNavwalker;
  */
 ?>
 <div id="MobileNavMenu" :class="[{ 'is-active': isOpen }, 'navbar']">
-    <?php wp_nav_menu(array(
+    <?php wp_nav_menu([
         'theme_location' => 'mobile-menu',
         'container'      => false,
         'menu_class'     => 'navbar-start',
@@ -20,39 +20,44 @@ use Includes\Modules\Navwalker\BulmaNavwalker;
         'link_after'     => '',
         'items_wrap'     => '<div id="%1$s" class="%2$s">%3$s</div>',
         'walker'         => new BulmaNavwalker()
-    )); ?>
+    ]); ?>
 </div>
 <div :class="['site-wrapper', { 'menu-open': isOpen }, {'full-height': footerStuck}]">
-<div class="site-mobile-overlay"></div>
-<header id="top" class="header">
-    <div class="container-fluid">
-        <nav class="navbar">
+    <div class="site-mobile-overlay"></div>
 
-            <div class="navbar-brand">
-                <a href="/">
-                    <img src="<?php echo get_template_directory_uri() . '/img/slim-logo.svg'; ?>" alt="Bulma: a modern CSS framework based on Flexbox" width="200" height="60">
-                </a>
+    <header id="top" class="header">
+        <div class="container">
+            <nav class="navbar">
 
-                <div class="navbar-burger burger" id="MobileNavBurger" data-target="MobileNavMenu" @click="toggleMenu">
-                    <span></span>
-                    <span></span>
-                    <span></span>
+                <div class="navbar-brand">
+                    <a href="/">
+                        <img src="<?= get_template_directory_uri() . '/img/psjumc-logo.svg'; ?>" alt="<?= get_bloginfo(); ?>" >
+                    </a>
+
+                    <div class="navbar-burger burger" id="MobileNavBurger" data-target="MobileNavMenu" @click="toggleMenu">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
                 </div>
-            </div>
 
-            <?php wp_nav_menu(array(
-                'theme_location' => 'main-menu',
-                'container'      => false,
-                'menu_class'     => 'navbar-end',
-                'fallback_cb'    => '',
-                'menu_id'        => 'main-menu',
-                'link_before'    => '',
-                'link_after'     => '',
-                'items_wrap'     => '<div id="%1$s" class="%2$s">%3$s</div>',
-                'walker'         => new BulmaNavwalker()
-            )); ?>
+                <?php wp_nav_menu([
+                    'theme_location' => 'main-menu',
+                    'container'      => false,
+                    'menu_class'     => 'navbar-end',
+                    'fallback_cb'    => '',
+                    'menu_id'        => 'main-menu',
+                    'link_before'    => '',
+                    'link_after'     => '',
+                    'items_wrap'     => '<div id="%1$s" class="%2$s">%3$s</div>',
+                    'walker'         => new BulmaNavwalker()
+                ]); ?>
 
-        </nav>
-    </div>
-</header>
+            </nav>
+
+        </div>
+    </header>
+
+
+
 
