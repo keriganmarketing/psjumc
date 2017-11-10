19712ca3b566ce5ab2e072f8b8425695e26af953
Jack Kerigan <support@kerigan.com>
2017-11-10 12:02:47 -0500

    news styles

--- wp-content/themes/kma-slim/template-parts/partials/mini-article.php
+++ wp-content/themes/kma-slim/template-parts/partials/mini-article.php
@@ -7,9 +7,28 @@ $photoUrl = $feed->photo($result);
 ?>
 <div class="card mini-article">
     <div class="card-image">
-        <figure class="image is-16by3">
-        <img src="<?= $photoUrl ?>">
-        </figure>
+        <?php if($result->type != 'video') { ?>
+            <figure class="image is-16by9">
+                <a href="<?php echo $result->link; ?>" target="_blank">
+                    <img src="<?php echo $photoUrl; ?>" alt="<?php echo $result->caption; ?>" >
+                </a>
+            </figure>
+        <?php } else { ?>
+            <figure class="image video is-16by9">
+                <iframe
+                        src="<?php echo 'https://www.facebook.com/plugins/video.php?href='.$result->link ?>"
+                        style="border:none;overflow:hidden"
+                        scrolling="no"
+                        frameborder="0"
+                        allowTransparency="true"
+                        allowFullScreen="true"
+                        class="article-image"
+                        width="100%"
+                        height="460">
+
+                </iframe>
+            </figure>
+        <?php } ?>
     </div>
     <div class="card-content">
         <?= ($headline!='' ? '<h3 class="title">'.$headline.'</h3>' : null); ?>
