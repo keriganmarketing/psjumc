d56d8c09d5b4f84fbc879868bfef1e97c7a2019b
Daron Adkins <daron.adkins@gmail.com>
2017-11-15 14:17:19 -0600

    Events improvements

--- wp-content/themes/kma-slim/template-parts/content-events.php
+++ wp-content/themes/kma-slim/template-parts/content-events.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use KeriganSolutions\FacebookFeed\FacebookEvents;
 
 /**
@@ -11,8 +12,11 @@ use KeriganSolutions\FacebookFeed\FacebookEvents;
 $headline = ($post->page_information_headline != '' ? $post->page_information_headline : $post->post_title);
 $subhead = ($post->page_information_subhead != '' ? $post->page_information_subhead : '');
 
-$feed    = new FacebookEvents();
-$results = $feed->fetch(9);
+$feed          = new FacebookEvents();
+$results       = $feed->fetch(9);
+$now           = Carbon::now('America/Chicago');
+$futureCounter = 0;
+$pastCounter   = 0;
 
 include(locate_template('template-parts/sections/top.php'));
 ?>
@@ -29,12 +33,45 @@ include(locate_template('template-parts/sections/top.php'));
                     <?php the_content(); ?>
                 </div>
 
+                <h2 class="title">Upcoming Events</h2>
                 <div class="columns is-multiline">
-                <?php foreach($results->data as $event){ ?>
-                    <div class="column is-6">
-                        <?php include(locate_template('template-parts/partials/mini-event.php')); ?>
-                    </div>
-                <?php } ?>
+                    <?php
+                    foreach ($results->data as $event) {
+                        if (Carbon::parse($event->start_time)->gt($now)) { ?>
+                        <div class="column is-6">
+                            <?php
+                                include(locate_template('template-parts/partials/mini-event.php'));
+                                $futureCounter++;
+                            ?>
+                        </div>
+                        <?php
+                        }
+                    }
+                    if ($futureCounter == 0) {
+                        echo '<div class="column is-6"><p>No events are currently scheduled</p></div><hr>';
+                    }
+                    ?>
+                </div>
+                <h2 class="title">Previous Events</h2>
+                <div class="columns is-multiline">
+                    <?php
+                    foreach ($results->data as $event) {
+                        if (Carbon::parse($event->start_time)->lt($now) && Carbon::parse($event->start_time)->gt($now->copy()->subMonths(6))) { ?>
+                        <div class="column is-6">
+                            <?php include(locate_template('template-parts/partials/mini-event.php'));
+                            $pastCounter++;
+                            ?>
+                        </div>
+                    <?php
+                        }
+                        if ($pastCounter >= 4) {
+                            break;
+                        }
+                    }
+                    if ($pastCounter == 0) {
+                        echo '<p>No recent events to display</p><hr>';
+                    }
+                    ?>
                 </div>
             </div>
         </div>
