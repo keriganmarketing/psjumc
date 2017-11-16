35c82ceea6d93544f329a9047e8ad4be1ce7f50c
Jack Kerigan <support@kerigan.com>
2017-11-16 17:24:35 -0500

    add arrows to sidebar buttons

--- wp-content/themes/kma-slim/template-parts/partials/salt-life-enewsletter.php
+++ wp-content/themes/kma-slim/template-parts/partials/salt-life-enewsletter.php
@@ -1,3 +1,3 @@
-<a href="/salt-life-newsletter/">
+<a href="/salt-life-newsletter/" class="image-button">
     <img class="photo-button" src="<?= get_template_directory_uri() . '/img/Newsletter@2x.png'; ?>" alt="ways to give to <?= get_bloginfo(); ?>" >
 </a>
