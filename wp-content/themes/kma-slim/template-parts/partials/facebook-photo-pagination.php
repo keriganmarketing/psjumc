4236e2689895e6c96e57abf7711a3c776265605b
Daron Adkins <daron.adkins@gmail.com>
2017-11-17 15:43:57 -0600

    fix facebook pagination

--- wp-content/themes/kma-slim/template-parts/partials/facebook-photo-pagination.php
+++ wp-content/themes/kma-slim/template-parts/partials/facebook-photo-pagination.php
@@ -1,17 +1,18 @@
 <?php
-    $disabledPrevious = isset($photos->paging->previous) ? false : true;
-    $disabledNext = isset($photos->paging->next) ? false : true;
+    $disabledPrevious = isset($photos->paging->previous) ? false: true;
+    $disabledNext     = isset($photos->paging->next) ? false: true;
+    $albumName        = $_GET['albumName'];
 ?>
 <nav class="pagination has-text-centered" role="navigation" aria-label="pagination">
     <a
         class="pagination-previous"
-        <?= ($disabledPrevious == false ? 'href="/album/?albumId='. $albumId . '&before=' . $photos->paging->cursors->before . '"' : ' disabled' ) ?>
+        <?= ($disabledPrevious == false ? 'href="/album/?albumId='. $albumId . '&albumName='. $albumName .  '&before=' . $photos->paging->cursors->before . '"' : ' disabled' ) ?>
     >
         Previous
     </a>
     <a
         class="pagination-next"
-        <?= ($disabledNext == false ? 'href="/album/?albumId='. $albumId . '&after=' . $photos->paging->cursors->after . '"' : ' disabled' ) ?>
+        <?= ($disabledNext == false ? 'href="/album/?albumId='. $albumId . '&albumName='. $albumName . '&after=' . $photos->paging->cursors->after . '"' : ' disabled' ) ?>
     >
         Next page
     </a>
